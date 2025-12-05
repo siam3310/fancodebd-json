1eb00f14944df4e75cb8583a2ebaab5131a30c7b
siam3310siam1 <siam3310siam1@gmail.com>
2025-12-05 07:36:41 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,55 +1,28 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
-
+  channel = "stable-23.11"; # Or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.nodejs_20 # For Node.js
+    pkgs.python311 # For Python
   ];
-
   # Sets environment variables in the workspace
   env = {};
+  # Search for the starship package in the nixpkgs channel and install it.
+  # Help -> Show connection status for more details
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      # "vscodevim.vim"
+    # Pre-warm the language servers for the following languages
+    # The values in this list should be one of the following:
+    # go, java, javascript, nix, python, rust, typescript
+    preWarm = [
     ];
-
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-
-    # Workspace lifecycle hooks
     workspace = {
-      # Runs when a workspace is first created
+      # Runs when a workspace is created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
+        npm-install = "npm install";
       };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
+      # Runs when a workspace is started
+      onStart = {};
     };
   };
 }
